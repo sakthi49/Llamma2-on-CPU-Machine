b50cc0c1ab1bd0f56ea2fd6aacad57b7c4587576
sakthi49 <sakthikumaran.1997@gmail.com>
2024-06-09 15:00:52 +0530

    app edit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Sakthikumaran',
+    author_email= 'sakthikumaran.1997@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
